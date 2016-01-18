f8537908f201fbf07c20dab2fba8b729e0929d00
Ken Wakita <wakita@is.titech.ac.jp>
2016-01-18 16:31:04 +0900

    Simplification #VBO, now, is only one
    前の版の作例ではふたつのVBOを使っていたが，それは無駄だった．単純化した．

--- sb05/sb05a2.py
+++ sb05/sb05a2.py
@@ -7,23 +7,20 @@ from OpenGL.arrays import vbo
 class W(SB05):
     program = None
 
-    vertex = np.array([
-         0.25, -0.25,  0.50, 1.0, 0.0, 0.0,
-        -0.25, -0.25,  0.50, 0.0, 1.0, 0.0,
-         0.00,  0.25,  0.50, 0.0, 0.0, 1.0], dtype=np.float32)
-
     def initializeGL(self):
         super().initializeGL('sb05a.shaders')
 
-        v, [position_b, color_b] = self.vertex, glGenBuffers(2)
+        v = np.array([
+             0.25, -0.25,  0.50, 1.0, 0.0, 0.0,
+            -0.25, -0.25,  0.50, 0.0, 1.0, 0.0,
+             0.00,  0.25,  0.50, 0.0, 0.0, 1.0], dtype=np.float32)
 
-        glBindBuffer(GL_ARRAY_BUFFER, position_b)
+        glBindBuffer(GL_ARRAY_BUFFER, glGenBuffers(1))
         glBufferData(GL_ARRAY_BUFFER, v.nbytes, v, GL_STATIC_DRAW)
+
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, None)
         glEnableVertexAttribArray(0)
 
-        glBindBuffer(GL_ARRAY_BUFFER, color_b)
-        glBufferData(GL_ARRAY_BUFFER, v.nbytes, v, GL_DYNAMIC_DRAW)
         glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
         glEnableVertexAttribArray(1)
 
