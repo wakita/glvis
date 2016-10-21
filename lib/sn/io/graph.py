1ec0c1325d94d15fe0d91c1f4779f9f2dbb4ee5e
Ken Wakita <wakita@is.titech.ac.jp>
2016-10-21 23:01:46 +0900

    sn.io.graph::read を refactor
    Changes to be committed:
    	modified:   graph.py

--- lib/sn/io/graph.py
+++ lib/sn/io/graph.py
@@ -3,18 +3,18 @@ from pathlib import PurePath
 import networkx as nx
 import numpy as np
 
-types = 'adjlist, multiline_adjlist, edgelist, gexf, gml, graph6, graphml, leda, pajek, shp, yaml'.split(', ')
-reader = dict(zip(types, [eval('nx.read_' + type) for type in types]))
+_reader = dict(zip(types,
+                   [eval('nx.read_' + type) for type in
+                    'adjlist, multiline_adjlist, edgelist, gexf, gml, graph6, graphml, leda, pajek, shp, yaml'.split(', ')]))
 
 # SparseGraph6 => graph6, GIS Shapefile => shp
 # D3-style .json?
 
 def read(path, *args, type=None):
-    global types, reader
     suffix = PurePath(path).suffix
-    if type == None and suffix[1:] in types: type = suffix[1:]
+    if type == None and suffix[1:] in _reader.keys(): type = suffix[1:]
     print(type)
-    if type in types:
+    if type in _reader.keys():
         return reader[type](path, *args)
 
 
