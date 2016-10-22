882f18f6b34af1af667a876ae9360ebaa5bf0e71
Ken Wakita <wakita@is.titech.ac.jp>
2016-10-22 15:13:42 +0900

    小さな修正。

--- lib/sn/io/graph.py
+++ lib/sn/io/graph.py
@@ -1,5 +1,7 @@
 from pathlib import PurePath, Path
 from glob import glob
+import sys
+import traceback
 
 import networkx as nx
 import numpy as np
@@ -9,8 +11,11 @@ _extensions = 'adjlist, multiline_adjlist, edgelist, gexf, gml, graph6, graphml,
 _reader = dict(zip(_extensions,
                    [eval('nx.read_' + type) for type in _extensions]))
 
+##
+# Todo
 # SparseGraph6 => graph6, GIS Shapefile => shp
 # D3-style .json?
+
 
 def read(path, *args, type=None, **keywords):
     p = PurePath(path)
@@ -21,20 +26,16 @@ def read(path, *args, type=None, **keywords):
             # label_path = dir.joinpath(stem + '.labels')
             # print(path)
             # print(label_path)
-            g = _reader[type](path, *args, label='id', **keywords)
+            if type == 'gml': keywords['label'] = 'id'
+            g = _reader[type](path, *args, **keywords)
             return g
-        except Exception as e:
-            print('type:', str(type(e)))
-            print('args:', str(e.args))
-            print('message:', e.message)
+        except:
+            print('Parsing failure')
+            e, message, trace = sys.exc_info()
+            print(message)
+            #traceback.print_tb(trace)
             return None
 
-
-er = nx.erdos_renyi_graph(100, 0.15)
-ws = nx.watts_strogatz_graph(30, 3, 0.1)
-ba = nx.barabasi_albert_graph(100, 5)
-
-networks = [er, ws, ba]
 
 centrality = dict(
     v=dict(
@@ -47,19 +48,7 @@ centrality = dict(
     e=dict(
         betweenness=nx.edge_betweenness_centrality))
 
-for g in networks:
-    cmap = centrality['v']['degree'](g)
-    np.save('g', np.array(list(cmap.values())))
 
-#read('/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/lesmis.gml', label='id')
-#nx.read_gml('/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/graphs/sierpinski04.gml', label='id')
-#nx.read_gml('/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/gdea_conf_paper_1995_2011-nolabel.gml', label='id')
-#nx.read_graphml('/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/erdos1graph-reduced.463-2.graphml', node_type='str')
-
-for path in glob('/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/**/*.gml', recursive=True):
+for path in glob('/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/**/*', recursive=True):
     g = read(path)
     if g: print(path, g.order())
-    else: print(path, 'Parsing failure')
-
-# failures
-# dolphins.gml, erdox1graph-reduced.463.graphml, gdea_conf_paper_1995_2011.gml, karate.am, lesmis.gml, *.graphml
