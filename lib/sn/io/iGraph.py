94eec7480aceaca2508cf5b0eeb0109b09a6083b
Ken Wakita <wakita@is.titech.ac.jp>
2016-12-06 17:23:45 +0900

    ラベルの読み込みに対応。

--- lib/sn/io/iGraph.py
+++ lib/sn/io/iGraph.py
@@ -1,5 +1,5 @@
 from pathlib import PurePath, Path
-from glob import glob
+#from glob import glob
 import json
 import re
 import sys
@@ -16,15 +16,14 @@ _DEBUG_ = False
 sn.utils._VERBOSE_ = True
 
 
-def read(path, *args, **kwds) -> Graph:
+def read(path: PurePath, *args, **kwds) -> Graph:
     try:
-        g = Graph.Read(path, *args, **kwds)
+        g = Graph.Read(str(path), *args, **kwds)
         benchmark(message='Reading "{0}"'.format(path))
         try:
-            p = PurePath(path)
-            print(p.parent.joinpath(p.stem + '.labels'))
-            with open(str(p.parent.joinpath(p.stem + '.labels'))) as r:
-                g.vs['Label'] = [ re.sub(r'\n', '', line) for line in r.readlines()]
+            print(path.parent.joinpath(path.stem + '.labels'))
+            with open(str(path.parent.joinpath(path.stem + '.labels'))) as r:
+                g.vs['label'] = [ re.sub(r'\n', '', line) for line in r.readlines()]
         except FileNotFoundError: pass
         return g
     except:
@@ -58,9 +57,10 @@ def normalize(g: Graph, profile: dict) -> Graph:
         assert not any(g.is_loop())
 
         io_array(adj_path, g.get_edgelist())
-        if 'Label' in g.vs.attributes():
+
+        if all([len(label) > 0 for label in g.vs['label']]):
             label_path = graph_dir.joinpath('label')
-            io_array(label_path, g.vs['Label'])
+            io_array(label_path, g.vs['label'])
         return g
 
 
@@ -86,29 +86,30 @@ if __name__ == '__main__' and False:
     print(g)
 
 
-def cmdscale(G, profile, force=False):
+def cmdscale(g: Graph, profile: dict):
     '''cmdscale: http://www.nervouscomputer.com/hfs/cmdscale-in-python/'''
+
     graph_dir = PurePath(profile['root']).joinpath(profile['name'])
     Λ_path, E_path = Path(graph_dir.joinpath('eigenvalues')), Path(graph_dir.joinpath('eigenvectors'))
-    if not force and Λ_path.exists() and E_path.exists():
+    if not profile['force'] and Λ_path.exists() and E_path.exists():
         return io_array(Λ_path), io_array(E_path)
 
     # Distance matrix (All-pairs shortest path length in numpy array)
     distance_file = graph_dir.joinpath('graph', 'distance.npy')
     try:
-        if not force:
+        if not profile['force']:
             raise FileNotFoundError
-        D = np.load(str(distance_file))
+        d = np.load(str(distance_file))
     except FileNotFoundError:
-        D = np.array([list(row.values()) for row in nx.all_pairs_shortest_path_length(G).values()], dtype=np.int)
-        io_array(str(distance_file), D)
-        if _DEBUG_ and G.number_of_nodes() < 100: print(D)
+        d = np.array([list(row.values()) for row in nx.all_pairs_shortest_path_length(g).values()], dtype=np.int)
+        io_array(str(distance_file), d)
+        if _DEBUG_ and g.number_of_nodes() < 100: print(d)
         benchmark(message='All-pairs shortest path length over the graph')
 
     # Classical Multi Dimensional Scaling
-    N, _ = D.shape
+    N, _ = d.shape
     J = np.eye(N) - np.ones((N, N)) / N      # Centering matrix
-    B = - J.dot(D * D).dot(J) / 2.0          # Apply double centering
+    B = - J.dot(d * d).dot(J) / 2.0          # Apply double centering
     Λ, E = np.linalg.eigh(B)
 
     Λ_positive = Λ > 0                       # Choose positive eigenvalues
@@ -139,45 +140,53 @@ def cmdscale(G, profile, force=False):
     return Λ, E
 
 
-def convert(root, path, force=False):
-    0
-
-
-def load(root, name):
-    0
-
-
-def centrality(G):
+def centrality(g: Graph, profile: dict):
 
     # Betweenness centrality
     # API: G.betweenness(vertices, directed=True, cutoff=None, weights=None, nobigint=True)
 
     # Closeness centrality
-    G.closeness(vertices=None, mode=ALL, cutoff=None, weights=None, normalized=True)
+    g.closeness(vertices=None, mode=ALL, cutoff=None, weights=None, normalized=True)
 
     # Clustering coefficient
-    G.transitivity_local_undirected(vertices=None, mode="nan", weights=None)
+    g.transitivity_local_undirected(vertices=None, mode="nan", weights=None)
 
     # Degree distribution
     # API: G.degree(G.vertices, loops=False) -- Degrees of 'vertices' for both incoming&outgoing direction (mode=ALL), ignoring self-loops
-    G.degree(loops=False)
+    g.degree(loops=False)
 
     # Eigenvector centrality
-    G.eigenvector_centrality(directed=True, scale=True, weights=None, return_eigenvalue=False, arpack_options=None)
+    g.eigenvector_centrality(directed=True, scale=True, weights=None, return_eigenvalue=False, arpack_options=None)
 
     # The Hub score for Kleinberg's HITS model
-    G.hub_score(weights=None, scale=True, arpack_options=None, return_eigenvalue=False)
+    g.hub_score(weights=None, scale=True, arpack_options=None, return_eigenvalue=False)
 
     # PageRank score
     #G.pagerank(self, vertices=None, directed=True, damping=0.85, weights=None, arpack_options=None, implementation='prpack', niter=1000, eps=0.001)
     #Calculates the Google PageRank values of a graph.	source code
 
     # Personalized PageRank score
-    G.personalized_pagerank(vertices=None, directed=True, damping=0.85, reset=None, reset_vertices=None, weights=None, arpack_options=None, implementation="prpack", niter=1000, eps=0.001)
+    g.personalized_pagerank(vertices=None, directed=True, damping=0.85, reset=None, reset_vertices=None, weights=None, arpack_options=None, implementation="prpack", niter=1000, eps=0.001)
 
 
     # Edge betweenness centrality
-    G.edge_betweenness(directed=True, cutoff=None, weights=None)
+    g.edge_betweenness(directed=True, cutoff=None, weights=None)
+
+
+def analyse(root: PurePath, path: PurePath, profile: dict):
+    g = normalize(read(path), profile)
+
+    dataset_dir = root.joinpath(profile['name'])
+
+    Λ, E = cmdscale(g, profile)
+    #centrality(g, profile)
+
+    misc_dir = dataset.joinpath('misc')
+    Path(misc_dir).mkdir(exist_ok=True, parents=True)
+    profile_path = misc_dir.joinpath('profile.p')
+    with open(str(profile_path), 'wb') as w:
+        pickle.dump(profile, w)
+    print(json.dumps(profile, indent = 4))
 
 
 if __name__ == '__main__':
@@ -187,9 +196,12 @@ if __name__ == '__main__':
     dataset = '/Users/wakita/Dropbox (smartnova)/work/glvis/data/takami-svf/'
     testcase = {
         'hypercube-4d': dataset + 'graphs/hypercube-4d.graphml',
-        'lesmis': dataset + 'lesmis.gml' }
+        'lesmis': dataset + 'lesmis.gml',
+        'math': dataset + 'math.wikipedia/math.graphml'
+    }
 
     for name, path in testcase.items():
-        g = normalize(read(path), dict(profile, name=name))
-        if 'Label' in g.vs.attributes(): print(g.vs['Label'][0:4])
+        g = normalize(read(PurePath(path)), dict(profile, name=name))
+        if all([len(label) > 0 for label in g.vs['label']]):
+            print(g.vs['label'][0:4])
         else: print('No labels')
